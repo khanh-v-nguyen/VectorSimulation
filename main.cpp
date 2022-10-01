fa1a25d9de8132900612d18c2abd44b068014f3b
khanh38 <68301461+khanh38@users.noreply.github.com>
2022-09-30 21:14:10 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "functions.h"
+
+int main() {
+    int size = 5;
+    std::string * dynamicArray = createArray();
+    addEntry(dynamicArray, size, "sara");
+    addEntry(dynamicArray, size, "rex");
+    addEntry(dynamicArray, size, "jerry");
+    deleteEntry(dynamicArray, size, "cindy");
+    deleteEntry(dynamicArray, size, "jimin");
+    deleteEntry(dynamicArray, size, "jimin");
+
+    for(int i = 0;  i < size; i++)
+    {
+        std::cout << dynamicArray[i] << " ";
+    }
+}
